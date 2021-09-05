7cbcda5dfa28331032e699794828e7030dbb17f6
aminabuljubasic <74607996+aminabuljubasic@users.noreply.github.com>
2021-09-05 18:12:25 +0200

    Update docker-tutorial-hello-project.cpp

--- src/docker-tutorial-hello-project.cpp
+++ src/docker-tutorial-hello-project.cpp
@@ -22,7 +22,6 @@ int main(int argc, char **argv) {
 			cout << "Directory is : " << directory << endl;
 			
 			filename_in = directory + "/read.txt";
-			cout<<filename_in<<endl;
 			in.open(filename_in.c_str());
 
 			if (!in.good()){
@@ -32,7 +31,6 @@ int main(int argc, char **argv) {
 
 
 			filename_out = directory + "/write.txt";
-			cout<<filename_out<<endl;
 			out.open(filename_out.c_str());
 
 
