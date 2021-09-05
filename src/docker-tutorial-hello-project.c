f0ca832cf65838ff795c1c8335af396e0c64e604
aminabuljubasic <74607996+aminabuljubasic@users.noreply.github.com>
2021-09-05 18:11:17 +0200

    Update docker-tutorial-hello-project.c

--- src/docker-tutorial-hello-project.c
+++ src/docker-tutorial-hello-project.c
@@ -15,14 +15,11 @@ int main(int argc, char **argv) {
       char directory_help[40];
       strcpy(directory_help,directory);
       filename_in = strcat(directory_help,"/read.txt");
-      printf("Filename in is : %s \n", filename_in);
       demo_read = fopen(filename_in,"r+");
       if(!demo_read){
-	      printf("UDJEM U IF \n");
          printf("You forgot the read.txt file in the bindmount directory, quitting. \n");
          exit(1);
       }
-      printf("POSLIJE IF-A \n");
       char directory_help2[40];
       strcpy(directory_help2,directory);
       filename_out = strcat(directory_help2,"/write.txt");
@@ -33,11 +30,11 @@ int main(int argc, char **argv) {
            fprintf(demo_write, "%c ",c);
       }
       fprintf(demo_write, "\n %s  ","Hello world");
-      printf("Hello world from the final program!  Here were the arguments!");
+      printf("Hello world from the final program!  Here were the arguments! \n");
     
       for (int i = 2; i < argc; i++){
 		  	printf("Argument %d %s \n",i,argv[i]);
-        fprintf(demo_write, "%s %d %s \n", "Arguments ",i,argv[i]);
+        		fprintf(demo_write, "%s %d %s \n", "Arguments ",i,argv[i]);
 	  	}
     fclose(demo_write);
     fclose(demo_read);
